33f916edcec305c73cbfec7400c75358aa42a52d
Daniel Tingelöf <daniel.tingelof@gmail.com>
2021-06-04 19:10:35 +0200

    Lint all files

--- templates/polybar/now_playing.py
+++ templates/polybar/now_playing.py
@@ -16,18 +16,22 @@ import sys
 
 try:
     session_bus = dbus.SessionBus()
-    spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+    spotify_bus = session_bus.get_object(
+        "org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
     spotify_properties = dbus.Interface(spotify_bus, "org.freedesktop.DBus.Properties")
     is_playing = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
     metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
 except Exception:
     sys.exit(1)
 
+
 def get_title():
     return metadata['xesam:title']
 
+
 def get_artist():
     return metadata['xesam:artist'][0]
+
 
 try:
     if is_playing == "Playing":
