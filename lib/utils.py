15a92cc4afb471e489d28707a46578280c53b252
Daniel Tingelöf <daniel.tingelof@gmail.com>
2021-10-02 22:55:55 +0200

    refactor rice

--- lib/utils.py
+++ lib/utils.py
@@ -8,7 +8,8 @@ def get_context(theme):
     """Load context of the given theme that should be used when ricing."""
 
     with open(f"themes/{theme}.json", "r") as colors_context, open(
-        "themes/settings.json", "r"
+        "themes/settings.json",
+        "r",
     ) as settings_context:
         theme_path = os.path.abspath(theme)
         print(f"Using context: '{theme_path}.json'")
@@ -18,20 +19,13 @@ def get_context(theme):
         return context
 
 
-def make_executable(file):
-    """Make given file executeable."""
-    mode = os.stat(file).st_mode
-    mode |= (mode & 0o444) >> 2  # copy R bits to X
-    os.chmod(file, mode)
-
-
 def get_wireless_if():
     """Read wireless data for obtaining wireless network interface."""
-    lines = open("/proc/net/wireless", "r").readlines()
-    try:
-        interface = lines[2:][0].split(":")[0]
-        print(f"Wireless network interface: {interface}")
+    with open("/proc/net/wireless", "r") as wireless:
+        try:
+            interface = wireless.readlines()[2:][0].split(":")[0]
+            print(f"Wireless network interface: {interface}")
 
-        return interface
-    except IndexError:
-        return "N/A"
+            return interface
+        except IndexError:
+            return "N/A"
