65b8c50ecf3b46d34ed52594c67f6db3b9bd267b
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2021-11-01 08:51:44 +0100

    Vim theme tweak
    Change-Id: Ia97d11a3be97159df500fc20b964b25059f158c6

--- templates/nvim/lua/lsp_config.lua
+++ templates/nvim/lua/lsp_config.lua
@@ -122,7 +122,7 @@ cmp.setup({
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.close(),
+        ["<C-x>"] = cmp.mapping.close(),
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
         ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
