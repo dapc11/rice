47c0fa697b2d919c7e51c7e1906271ebbd8e947b
Daniel Tingelöf <daniel.tingelof@gmail.com>
2021-12-10 22:41:37 +0100

    [Nvim] Cleanup

--- templates/nvim/lua/options.lua
+++ templates/nvim/lua/options.lua
@@ -1,50 +1,49 @@
-local set_global = vim.g
-local set = vim.opt
+local g, s = vim.g, vim.opt
 
-set.tabstop        = 4
-set.softtabstop    = 4
-set.shiftwidth     = 4               -- 4 spaces
-set.shiftround     = true            -- Round tabs to multiplier of shiftwicth
-set.smartindent    = true
-set.expandtab      = true            -- In Insert mode: Use the appropriate number of spaces to insert a tab
-set.relativenumber = true            -- relative line numbers to current line
-set.cursorline     = true            -- Highlgiht cursor line
-set.hlsearch       = true            -- Highlight search
-set.hidden         = true
-set.errorbells     = false           -- No sound on error
-set.nu             = true            -- Line numbers
-set.wrap           = false
-set.swapfile       = false
-set.backup         = false
-set.undodir        = os.getenv("HOME") .. "/.vim/undodir"
-set.undofile       = true
-set.incsearch      = true            -- Evolve search as I write
-set.termguicolors  = true            -- Make colorscheme work
-set.scrolloff      = 8               -- Start scroll when n lines from screen edge
-set.showmode       = false
-set.colorcolumn    = "100"           -- Dont go further
-set.updatetime     = 50              -- Short time to combo key strokes
-set.mouse          = "a"             -- Enable mouse
-set.autoread       = true
-set.completeopt    = "menu,menuone,noselect"
-set.shortmess:append("c")
-set.clipboard:append("unnamedplus")   -- System clipboard
+s.tabstop        = 4
+s.softtabstop    = 4
+s.shiftwidth     = 4               -- 4 spaces
+s.shiftround     = true            -- Round tabs to multiplier of shiftwicth
+s.smartindent    = true
+s.expandtab      = true            -- In Insert mode: Use the appropriate number of spaces to insert a tab
+s.relativenumber = true            -- relative line numbers to current line
+s.cursorline     = true            -- Highlgiht cursor line
+s.hlsearch       = true            -- Highlight search
+s.hidden         = true
+s.errorbells     = false           -- No sound on error
+s.nu             = true            -- Line numbers
+s.wrap           = false
+s.swapfile       = false
+s.backup         = false
+s.undodir        = os.getenv("HOME") .. "/.vim/undodir"
+s.undofile       = true
+s.incsearch      = true            -- Evolve search as I write
+s.termguicolors  = true            -- Make colorscheme work
+s.scrolloff      = 8               -- Start scroll when n lines from screen edge
+s.showmode       = false
+s.colorcolumn    = "100"           -- Dont go further
+s.updatetime     = 50              -- Short time to combo key strokes
+s.mouse          = "a"             -- Enable mouse
+s.autoread       = true
+s.completeopt    = "menu,menuone,noselect"
+s.shortmess:append("c")
+s.clipboard:append("unnamedplus")   -- System clipboard
 
 -- Splits open at the bottom and right, which is non-retarded, unlike vim defaults.
-set.splitbelow = true
-set.splitright = true
+s.splitbelow = true
+s.splitright = true
 
-set_global["UltiSnipsExpandTrigger"] = "<tab>"
-set_global["UltiSnipsJumpForwardTrigger"] = "<tab>"
-set_global["UltiSnipsJumpBackwardTrigger"] = "<s-tab>"
-set_global["UltiSnipsSnippetDirectories"] = {"~/.config/nvim/UltiSnips/"}
+g["UltiSnipsExpandTrigger"] = "<tab>"
+g["UltiSnipsJumpForwardTrigger"] = "<tab>"
+g["UltiSnipsJumpBackwardTrigger"] = "<s-tab>"
+g["UltiSnipsSnippetDirectories"] = {"~/.config/nvim/UltiSnips/"}
 
-set_global["indent_blankline_use_treesitter"] = true
-set_global["indent_blankline_show_first_indent_level"] = true
-set_global["indent_blankline_filetype_exclude"] = {"help"}
-set_global["indentLine_setConceal"] = 0
+g["indent_blankline_use_treesitter"] = true
+g["indent_blankline_show_first_indent_level"] = true
+g["indent_blankline_filetype_exclude"] = {"help"}
+g["indentLine_setConceal"] = 0
 
-set_global["rooter_patterns"] = {"setup.cfg", ".git", "pom.xml", "Makefile", "*.sln", "build/env.sh"}
+g["rooter_patterns"] = {"setup.cfg", ".git", "pom.xml", "Makefile", "*.sln", "build/env.sh"}
 -- vim.g["rooter_change_directory_for_non_project_files"] = "home"
 
-set_global["python3_host_prog"] = "~/dev/bin/python3"
+g["python3_host_prog"] = "~/dev/bin/python3"
