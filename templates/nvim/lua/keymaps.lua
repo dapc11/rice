f747f966689340775ac94bef5c626e90a816252e
Daniel Tingelöf <daniel.tingelof@gmail.com>
2021-12-10 19:37:02 +0100

    [Vim] Add command for capturing command output

--- templates/nvim/lua/keymaps.lua
+++ templates/nvim/lua/keymaps.lua
@@ -202,3 +202,37 @@ vim.cmd[[
     cnoremap <expr> <left> wildmenumode() ? "\<up>" : "\<left>"
     cnoremap <expr> <right> wildmenumode() ? " \<bs>\<C-Z>" : "\<right>"
 ]]
+vim.cmd[[
+    function! s:split(expr) abort
+    let lines = split(execute(a:expr, 'silent'), "[\n\r]")
+    let name = printf('capture://%s', a:expr)
+
+    if bufexists(name) == v:true
+        execute 'bwipeout' bufnr(name)
+    end
+
+    execute 'botright' 'new' name
+
+    setlocal buftype=nofile
+    setlocal bufhidden=hide
+    setlocal noswapfile
+    setlocal filetype=vim
+
+    call append(line('$'), lines)
+    endfunction
+
+    function! s:fzf(expr) abort
+    let lines = split(execute(a:expr, 'silent'), "[\n\r]")
+    return fzf#run({'source': lines,  'options': '--tiebreak begin --ansi --header-lines 1'})
+    endfunction
+
+    function s:capture(expr, bang) abort
+        if a:bang
+            call s:fzf(a:expr)
+        else
+            call s:split(a:expr)
+            endif
+            endfunction
+
+    command! -nargs=1 -bang -complete=command P call s:capture(<q-args>, <bang>0)
+]]
