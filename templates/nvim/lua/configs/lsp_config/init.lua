520fd2cca985641e199a7c4533d97bb01d123778
Daniel Tingelöf <daniel.tingelof@gmail.com>
2022-02-05 21:16:30 +0100

    Lsp config fixes

--- templates/nvim/lua/configs/lsp_config/init.lua
+++ templates/nvim/lua/configs/lsp_config/init.lua
@@ -1,50 +1,97 @@
-local nvim_lsp = require("lspconfig")
-
-local border = {
-{"╭", "FloatBorder"},
-{"─", "FloatBorder"},
-{"╮", "FloatBorder"},
-{"│", "FloatBorder"},
-{"╯", "FloatBorder"},
-{"─", "FloatBorder"},
-{"╰", "FloatBorder"},
-{"│", "FloatBorder"},
-}
-local handlers =  {
-  ["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = "single", scope = "line"}),
-  ["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = "single", scope = "line" }),
-}
-
--- To instead override globally
-local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-  opts = opts or {}
-  opts.border = opts.border or border
-  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+local status_ok, lspconfig = pcall(require, "lspconfig")
+if not status_ok then
+    return
+end
+local status_ok, lspsignature = pcall(require, "lsp_signature")
+if not status_ok then
+    return
+end
+local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not status_ok then
+    return
 end
 
------- Setup lsp_config.
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-    require "lsp_signature".on_attach({
-        bind = true, -- This is mandatory, otherwise border config won"t get registered.
-        handler_opts = {
-            border = "single"
-        },
-        hint_prefix = " ",
-        max_height = 8,
-    }, bufnr)
+local signs = {
+    {
+        name = "DiagnosticSignError",
+        text = "",
+        type = "Error"
+    }, {
+        name = "DiagnosticSignWarn",
+        text = "",
+        type = "Warn"
+    }, {
+        name = "DiagnosticSignHint",
+        text = "",
+        type = "Hint"
+    }, {
+        name = "DiagnosticSignInfo",
+        text = "",
+        type = "Info"
+    }
+}
 
-    -- Enable completion triggered by <c-x><c-o>
-    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+for _, sign in ipairs(signs) do
+    local hl = "DiagnosticLineNr" .. sign.type
+    vim.fn.sign_define(sign.name, {
+        texthl = sign.name,
+        text = sign.text,
+        numhl = hl
+    })
+end
 
-    -- Mappings.
-    local opts = { noremap=true, silent=true }
+vim.cmd [[
+  highlight DiagnosticLineNrError guifg={{base08}} gui=bold
+  highlight DiagnosticLineNrWarn guifg={{base09}} gui=bold
+  highlight DiagnosticLineNrInfo guifg={{base0C}} gui=bold
+  highlight DiagnosticLineNrHint guifg={{base0D}} gui=bold
 
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
+  sign define DiagnosticSignError text= texthl=DiagnosticSignError linehl= numhl=DiagnosticLineNrError
+  sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn linehl= numhl=DiagnosticLineNrWarn
+  sign define DiagnosticSignInfo text= texthl=DiagnosticSignInfo linehl= numhl=DiagnosticLineNrInfo
+  sign define DiagnosticSignHint text= texthl=DiagnosticSignHint linehl= numhl=DiagnosticLineNrHint
+]]
+local config = {
+    virtual_text = false,
+    signs = {
+        active = signs
+    },
+    update_in_insert = true,
+    underline = true,
+    severity_sort = true,
+    float = {
+        focusable = false,
+        style = "minimal",
+        border = "single",
+        source = "always",
+        header = "",
+        prefix = ""
+    }
+}
+
+vim.diagnostic.config(config)
+
+local handlers = {
+    ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+        border = "single",
+        scope = "line"
+    }),
+    ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+        border = "single",
+        scope = "line"
+    })
+}
+
+local function lsp_keymaps(bufnr)
+    local function buf_set_keymap(...)
+        vim.api.nvim_buf_set_keymap(bufnr, ...)
+    end
+
+    local opts = {
+        noremap = true,
+        silent = true
+    }
+
     buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
     buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
     buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
@@ -58,81 +105,81 @@ local on_attach = function(client, bufnr)
     buf_set_keymap("n", "<C-n>", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
 end
 
+local function lsp_highlight_document(client)
+    if client.resolved_capabilities.document_highlight then
+        vim.api.nvim_exec([[
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]], false)
+    end
+end
+
+local function lsp_signature(bufnr)
+    lspsignature.on_attach({
+        bind = true, -- This is mandatory, otherwise border config won"t get registered.
+        handler_opts = {
+            border = "single"
+        },
+        hint_prefix = " ",
+        max_height = 8
+    }, bufnr)
+
+end
+
+local on_attach = function(client, bufnr)
+    lsp_signature(bufnr)
+    lsp_keymaps(bufnr)
+    lsp_highlight_document(client)
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+
+local flags = {
+    debounce_text_changes = 150
+}
+
 -- Use a loop to conveniently call "setup" on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { "gopls" }
+local servers = {"gopls"}
 for _, lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup {
+    lspconfig[lsp].setup {
         on_attach = on_attach,
         handlers = handlers,
-        capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-        flags = {
-            debounce_text_changes = 150,
-        }
+        capabilities = capabilities,
+        flags = flags
     }
 end
 
 local util = require("lspconfig/util")
-nvim_lsp.pyright.setup {
+lspconfig.pyright.setup {
     on_attach = on_attach,
-    flags = {
-        debounce_text_changes = 150,
-    },
     handlers = handlers,
-    capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    capabilities = capabilities,
+    flags = flags,
     settings = {
         python = {
             analysis = {
-                extraPaths = {"/home/daniel/repos/dapc11-rice/"},
                 autoSearchPaths = true,
                 useLibraryCodeForTypes = true,
-                diagnosticMode = 'openFilesOnly',
-            },
-        },
+                diagnosticMode = 'openFilesOnly'
+            }
+        }
     },
     root_dir = function(fname)
-        return util.root_pattern(".git", "setup.py",  "setup.cfg", "pyproject.toml", "requirements.txt")(fname) or
-        util.path.dirname(fname)
-    end,
+        return util.root_pattern(".git", "setup.py", "setup.cfg", "pyproject.toml", "requirements.txt")(fname) or
+                   util.path.dirname(fname)
+    end
 }
-
-vim.cmd [[
-  highlight DiagnosticLineNrError guifg={{base08}} gui=bold
-  highlight DiagnosticLineNrWarn guifg={{base09}} gui=bold
-  highlight DiagnosticLineNrInfo guifg={{base0C}} gui=bold
-  highlight DiagnosticLineNrHint guifg={{base0D}} gui=bold
-
-  sign define DiagnosticSignError text= texthl=DiagnosticSignError linehl= numhl=DiagnosticLineNrError
-  sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn linehl= numhl=DiagnosticLineNrWarn
-  sign define DiagnosticSignInfo text= texthl=DiagnosticSignInfo linehl= numhl=DiagnosticLineNrInfo
-  sign define DiagnosticSignHint text= texthl=DiagnosticSignHint linehl= numhl=DiagnosticLineNrHint
-]]
-
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
-
-vim.diagnostic.config({
-    underline = true,
-    virtual_text = false,
-    signs = true,
-    severity_sort = true,
-})
-
-vim.fn.sign_define("DiagnosticSignError", { text = "✗", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = "!", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInformation", { text = "", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
-
 
 local win = require('lspconfig.ui.windows')
 local _default_opts = win.default_opts
 
 win.default_opts = function(options)
-  local opts = _default_opts(options)
-  opts.border = 'single'
-  return opts
+    local opts = _default_opts(options)
+    opts.border = 'single'
+    return opts
 end
--- templates/nvim/lua/configs/telescope/init.lua
+++ templates/nvim/lua/configs/telescope/init.lua
@@ -1,19 +1,22 @@
-local telescope = require("telescope")
+local status_ok, telescope = pcall(require, "telescope")
+if not status_ok then
+    return
+end
+
 local actions = require("telescope.actions")
 local action_layout = require("telescope.actions.layout")
 
+telescope.load_extension("fzf")
+telescope.load_extension("projects")
+
 telescope.setup {
     defaults = {
-        file_ignore_patterns = { "node_modules", ".git" },
-        vimgrep_arguments = {
-            "rg",
-            "--no-heading",
-            "--color=never",
-            "--with-filename",
-            "--line-number",
-            "--column",
-            "--ignore-case",
-            "--trim" -- add this value
+        prompt_prefix = " ",
+        selection_caret = "❯ ",
+        path_display = {"smart"},
+        file_ignore_patterns = {"node_modules", ".git"},
+        vimgrep_arguments = {"rg", "--no-heading", "--color=never", "--with-filename", "--line-number", "--column",
+                             "--ignore-case", "--trim" -- add this value
         },
         mappings = {
             n = {
@@ -21,14 +24,20 @@ telescope.setup {
                 ["<M-q>"] = actions.send_to_qflist + actions.open_qflist,
                 ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
                 ["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
+                ["<C-c>"] = actions.close
             },
             i = {
                 ["<C-p>"] = action_layout.toggle_preview,
+                ["<C-c>"] = actions.close,
                 ["<esc>"] = actions.close,
                 ["<M-q>"] = actions.send_to_qflist + actions.open_qflist,
                 ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
                 ["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
-            },
+                ["<CR>"] = actions.select_default,
+                ["<C-x>"] = actions.select_horizontal,
+                ["<C-v>"] = actions.select_vertical,
+                ["<C-t>"] = actions.select_tab
+            }
         },
         preview = {
             filesize_hook = function(filepath, bufnr, opts)
@@ -43,9 +52,7 @@ telescope.setup {
             fuzzy = true,
             override_generic_sorter = true,
             override_file_sorter = true,
-            case_mode = "ignore_case",        -- or "ignore_case" or "respect_case"
+            case_mode = "ignore_case" -- or "ignore_case" or "respect_case"
         }
     }
 }
-telescope.load_extension("fzf")
-telescope.load_extension("projects")
