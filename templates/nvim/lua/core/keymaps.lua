faa94e7f42e87d8948be88234d3d98ea7464d0b5
Daniel Tingelöf <daniel.tingelof@gmail.com>
2022-02-24 20:36:37 +0100

    Fixes

--- templates/nvim/lua/core/keymaps.lua
+++ templates/nvim/lua/core/keymaps.lua
@@ -114,8 +114,6 @@ map {'t', '<Esc>', '<C-\\><C-n>'}
 map {"v", "p", '"_dP'}
 
 map {'n', '<C-Space>', '<C-x><C-n>'}
-map {'n', '<S-Tab>', ':bp<CR>'}
-map {'n', '<Tab>', ':bn<CR>'}
 
 -- " Requires gvim
 -- " paste with shift+insert
@@ -284,6 +282,4 @@ vim.api.nvim_set_keymap('', 't', "<cmd>lua require'hop'.hint_char1({ direction =
 vim.api.nvim_set_keymap('', 'T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
 
 
-map {'n', '<leader>ot', '<cmd>AerialToggle<CR>'}
-map {'n', '<leader>oo', '<cmd>AerialTreeOpen<CR>'}
-map {'n', '<leader>oc', '<cmd>AerialTreeClose<CR>'}
+map {'n', '<C-e>', '<cmd>AerialToggle<CR>'}
