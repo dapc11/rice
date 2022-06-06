36d1cd363b862b99b47832eab304623cdd745046
Daniel Tingelöf <daniel.tingelof@gmail.com>
2022-06-06 20:52:17 +0200

    Refactor rules and extract more keybinds

--- templates/awesome/keybinds.lua
+++ templates/awesome/keybinds.lua
@@ -205,4 +205,55 @@ keybinds.clientkeys = gears.table.join(
 		group = "client",
 	})
 )
+-- Bind all key numbers to tags.
+-- Be careful: we use keycodes to make it work on any keyboard layout.
+-- This should map on the top row of your keyboard, usually 1 to 9.
+for i = 1, 9 do
+	keybinds.globalkeys = gears.table.join(
+		keybinds.globalkeys, -- View tag only.
+		awful.key({ theme.modkey }, "#" .. i + 9, function()
+			local screen = awful.screen.focused()
+			local tag = screen.tags[i]
+			if tag then
+				tag:view_only()
+			end
+		end, {
+			description = "view tag #" .. i,
+			group = "tag",
+		}), -- Toggle tag display.
+		awful.key({ theme.modkey, "Control" }, "#" .. i + 9, function()
+			local screen = awful.screen.focused()
+			local tag = screen.tags[i]
+			if tag then
+				awful.tag.viewtoggle(tag)
+			end
+		end, {
+			description = "toggle tag #" .. i,
+			group = "tag",
+		}), -- Move client to tag.
+		awful.key({ theme.modkey, "Shift" }, "#" .. i + 9, function()
+			if client.focus then
+				local tag = client.focus.screen.tags[i]
+				if tag then
+					client.focus:move_to_tag(tag)
+				end
+			end
+		end, {
+			description = "move focused client to tag #" .. i,
+			group = "tag",
+		}), -- Toggle tag on focused client.
+		awful.key({ theme.modkey, "Control", "Shift" }, "#" .. i + 9, function()
+			if client.focus then
+				local tag = client.focus.screen.tags[i]
+				if tag then
+					client.focus:toggle_tag(tag)
+				end
+			end
+		end, {
+			description = "toggle focused client on tag #" .. i,
+			group = "tag",
+		})
+	)
+end
+
 return keybinds
--- templates/awesome/rc.lua
+++ templates/awesome/rc.lua
@@ -66,7 +66,6 @@ if not beautiful.init(theme_path) then
 end
 local theme = require("theme")
 local terminal = "alacritty"
-local modkey = "Mod4"
 
 awful.layout.layouts = {
 	awful.layout.suit.tile,
@@ -198,73 +197,19 @@ end)
 
 root.buttons(gears.table.join(awful.button({}, 4, awful.tag.viewnext), awful.button({}, 5, awful.tag.viewprev)))
 
-local keybinds = require("keybinds")
-
--- Bind all key numbers to tags.
--- Be careful: we use keycodes to make it work on any keyboard layout.
--- This should map on the top row of your keyboard, usually 1 to 9.
-local globalkeys
-for i = 1, 9 do
-	globalkeys = gears.table.join(
-		keybinds.globalkeys, -- View tag only.
-		awful.key({ modkey }, "#" .. i + 9, function()
-			local screen = awful.screen.focused()
-			local tag = screen.tags[i]
-			if tag then
-				tag:view_only()
-			end
-		end, {
-			description = "view tag #" .. i,
-			group = "tag",
-		}), -- Toggle tag display.
-		awful.key({ modkey, "Control" }, "#" .. i + 9, function()
-			local screen = awful.screen.focused()
-			local tag = screen.tags[i]
-			if tag then
-				awful.tag.viewtoggle(tag)
-			end
-		end, {
-			description = "toggle tag #" .. i,
-			group = "tag",
-		}), -- Move client to tag.
-		awful.key({ modkey, "Shift" }, "#" .. i + 9, function()
-			if client.focus then
-				local tag = client.focus.screen.tags[i]
-				if tag then
-					client.focus:move_to_tag(tag)
-				end
-			end
-		end, {
-			description = "move focused client to tag #" .. i,
-			group = "tag",
-		}), -- Toggle tag on focused client.
-		awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9, function()
-			if client.focus then
-				local tag = client.focus.screen.tags[i]
-				if tag then
-					client.focus:toggle_tag(tag)
-				end
-			end
-		end, {
-			description = "toggle focused client on tag #" .. i,
-			group = "tag",
-		})
-	)
-end
-
 local clientbuttons = gears.table.join(
 	awful.button({}, 1, function(c)
 		c:emit_signal("request::activate", "mouse_click", {
 			raise = true,
 		})
 	end),
-	awful.button({ modkey }, 1, function(c)
+	awful.button({ theme.modkey }, 1, function(c)
 		c:emit_signal("request::activate", "mouse_click", {
 			raise = true,
 		})
 		awful.mouse.client.move(c)
 	end),
-	awful.button({ modkey }, 3, function(c)
+	awful.button({ theme.modkey }, 3, function(c)
 		c:emit_signal("request::activate", "mouse_click", {
 			raise = true,
 		})
@@ -272,9 +217,20 @@ local clientbuttons = gears.table.join(
 	end)
 )
 
--- Set keys
-root.keys(globalkeys)
+local keybinds = require("keybinds")
+root.keys(keybinds.globalkeys)
 
+local function rule(class, screen, tag)
+	return {
+		rule = {
+			class = class,
+		},
+		properties = {
+			screen = screen,
+			tag = tag,
+		},
+	}
+end
 --  Rules
 -- Rules to apply to new clients (through the "manage" signal).
 awful.rules.rules = { -- All clients will match this rule.
@@ -335,132 +291,19 @@ awful.rules.rules = { -- All clients will match this rule.
 			titlebars_enabled = theme.titlebars_enabled,
 		},
 	},
-	{
-		rule = {
-			class = "Google-chrome",
-		},
-		properties = {
-			screen = 1,
-			tag = "2",
-		},
-	},
-	{
-		rule = {
-			class = "Firefox",
-		},
-		properties = {
-			screen = 1,
-			tag = "2",
-		},
-	},
-	{
-		rule = {
-			class = "Evolution",
-		},
-		properties = {
-			screen = 1,
-			tag = "1",
-		},
-	},
-	{
-		rule = {
-			class = "Jetbrains-idea-ce",
-		},
-		properties = {
-			screen = 1,
-			tag = "3",
-		},
-	},
-	{
-		rule = {
-			class = "Jetbrains-pycharm-ce",
-		},
-		properties = {
-			screen = 1,
-			tag = "3",
-		},
-	},
-	{
-		rule = {
-			class = "Wfica",
-		},
-		properties = {
-			screen = 1,
-			tag = "6",
-		},
-	},
-	{
-		rule = {
-			class = "wfica",
-		},
-		properties = {
-			screen = 1,
-			tag = "6",
-		},
-	},
-	{
-		rule = {
-			class = "Libreoffice-writer",
-		},
-		properties = {
-			screen = 1,
-			tag = "7",
-		},
-	},
-	{
-		rule = {
-			class = "Thunar",
-		},
-		properties = {
-			screen = 1,
-			tag = "",
-		},
-	},
-	{
-		rule = {
-			class = "PulseUi",
-		},
-		properties = {
-			screen = 1,
-			tag = "8",
-		},
-	},
-	{
-		rule = {
-			class = "Pidgin",
-		},
-		properties = {
-			screen = 1,
-			tag = "9",
-		},
-	},
-	{
-		rule = {
-			class = "Telegram",
-		},
-		properties = {
-			screen = 1,
-			tag = "9",
-		},
-	},
-	{
-		rule = {
-			class = "Teams-for-linux",
-		},
-		properties = {
-			screen = 1,
-			tag = "9",
-		},
-	},
-	{
-		rule = {
-			name = "Microsoft Teams Notification",
-		},
-		properties = {
-			screen = 1,
-			tag = "9",
-		},
-	},
+	rule("Google-chrome", 1, "2"),
+	rule("Firefox", 1, "2"),
+	rule("Evolution", 1, "1"),
+	rule("Jetbrains-idea-ce", 1, "3"),
+	rule("Jetbrains-pycharm-ce", 1, "3"),
+	rule("wfica", 1, "6"),
+	rule("Libreoffice-writer", 1, "7"),
+	rule("Thunar", 1, 1),
+	rule("PulseUi", 1, "8"),
+	rule("Pidgin", 1, "9"),
+	rule("Telegram", 1, "9"),
+	rule("Teams-for-linux", 1, "9"),
+	rule("Microsoft Teams Notification", 1, "9"),
 }
 
 --  Signals
