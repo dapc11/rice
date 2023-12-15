b50190de891c96042f4b9c7e639f522ae0c5e575
Daniel Tingelöf <daniel.tingelof@gmail.com>
2023-12-15 20:39:15 +0100

    fix(lint): Pylint warnings

--- lib/ThemeController.py
+++ lib/ThemeController.py
@@ -11,7 +11,7 @@ class ThemeController:  # pylint: disable=R0903
         self.theme = args.theme
 
     @staticmethod
-    def _get_themes():
+    def get_themes():
         """Visualize which themes that are available."""
         theme_names = []
 
@@ -22,19 +22,19 @@ class ThemeController:  # pylint: disable=R0903
 
         return theme_names
 
-    def _print_available_themes(self) -> None:
+    def print_available_themes(self) -> None:
         print("Specify theme with --theme (-t) flag.\nAvailable themes:")
 
-        for theme in self._get_themes():
+        for theme in self.get_themes():
             print(f"  - {theme}")
         sys.exit(0)
 
-    def _validate_theme(self, themes) -> bool:
+    def validate_theme(self, themes) -> bool:
         return self.theme is not None and self.theme in themes
 
     def get_theme(self) -> str:
         """Get theme if valid, otherwise print list of available themes."""
-        themes = self._get_themes()
-        if not self._validate_theme(themes) or self.list_themes:
-            self._print_available_themes()
+        themes = self.get_themes()
+        if not self.validate_theme(themes) or self.list_themes:
+            self.print_available_themes()
         return self.theme
